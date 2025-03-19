91cc07514203f297ee3d56a4af13d9e500380ae7
nukri060 <127565043+nukri060@users.noreply.github.com>
2025-03-19 15:55:21 +0400

    Implement snapFingers function with scoped checkStones function

---
+++ University Tasks/javascript-exercises/22_functions/infinityGauntlet.js
@@ -0,0 +1,12 @@
+function snapFingers() {
+    let stones = 6;
+  
+    function checkStones() {
+      console.log(`Thanos has ${stones} stones. The universe trembles!`);
+    }
+  
+    checkStones();
+}
+  
+snapFingers();
+  
